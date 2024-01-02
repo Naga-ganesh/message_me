ce742594c58374cba4747b62dd65f336c97b51ab
nagaganeshwar <naga.ganeshwar@chronus.com>
2024-01-02 22:10:52 +0530

    added functionality to user login logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
   root 'chatroom#index'
-  get '/login', to: 'sessions#new'
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete "logout", to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
